4dd13d727ceea1f3f2a7e90b4126f767db624bac
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-06-01 13:08:37 -0400

    feat: procedurally generated rope
    using a series of gameObjects connected by config joints and a line renderer to create one rope

---
+++ VR Surgery/Assets/RopeGeneration.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeGeneration : MonoBehaviour
+{
+    public GameObject ropePointPref;
+    public int numberPoints;
+    public float pointSpacing;
+
+    public GameObject[] ropePoints;
+
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ropePoints = new GameObject[numberPoints];
+
+        Vector3 currPos = transform.position;
+
+        for (int i = 0; i < numberPoints; i ++)
+        {
+            currPos += new Vector3(pointSpacing, 0, 0);
+
+            GameObject go = Instantiate(ropePointPref, currPos, Quaternion.identity);
+            go.transform.parent = transform;
+
+            ropePoints[i] = go;
+        }
+
+        ropePoints[0].GetComponent<ConfigurableJoint>().connectedBody = ropePoints[1].GetComponent<Rigidbody>();
+        ropePoints[0].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+        for (int i = 1; i < ropePoints.Length; i ++)
+        {
+            ropePoints[i].GetComponent<ConfigurableJoint>().connectedBody = ropePoints[i - 1].GetComponent<Rigidbody>();
+            
+        }
+
+        lineRenderer.positionCount = numberPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ropePoints.Length; i ++)
+        {
+            lineRenderer.SetPosition(i, ropePoints[i].transform.position);
+        }
+    }
+}
